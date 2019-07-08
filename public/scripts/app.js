67efb0a03646b51492667eaba5953ca39d97a4f5
Mohammad Moien <mnmoienian@gmail.com>
2019-07-08 12:13:57 +0430

    add handlePick function for handle pick option randomly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,6 +17,7 @@ var IndecisionApp = function (_React$Component) {
     var _this = _possibleConstructorReturn(this, (IndecisionApp.__proto__ || Object.getPrototypeOf(IndecisionApp)).call(this, props));
 
     _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
+    _this.handlePick = _this.handlePick.bind(_this);
     _this.state = {
       options: ["thing one", "thing two", "thing four"]
     };
@@ -33,6 +34,13 @@ var IndecisionApp = function (_React$Component) {
       });
     }
   }, {
+    key: "handlePick",
+    value: function handlePick() {
+      var randomNum = Math.floor(Math.random() * this.state.options.length);
+      var option = this.state.options[randomNum];
+      alert(option);
+    }
+  }, {
     key: "render",
     value: function render() {
       var title = "Indecision";
@@ -42,7 +50,10 @@ var IndecisionApp = function (_React$Component) {
         "div",
         null,
         React.createElement(Header, { title: title, subtitle: subtitle }),
-        React.createElement(Action, { hasOptions: this.state.options.length > 0 }),
+        React.createElement(Action, {
+          hasOptions: this.state.options.length > 0,
+          handlePick: this.handlePick
+        }),
         React.createElement(Options, {
           options: this.state.options,
           handleDeleteOptions: this.handleDeleteOptions
@@ -97,11 +108,6 @@ var Action = function (_React$Component3) {
   }
 
   _createClass(Action, [{
-    key: "handlePick",
-    value: function handlePick() {
-      alert("handlePick");
-    }
-  }, {
     key: "render",
     value: function render() {
       return React.createElement(
@@ -109,7 +115,10 @@ var Action = function (_React$Component3) {
         null,
         React.createElement(
           "button",
-          { onClick: this.handlePick, disabled: !this.props.hasOptions },
+          {
+            onClick: this.props.handlePick,
+            disabled: !this.props.hasOptions
+          },
           "What should I do?"
         )
       );
