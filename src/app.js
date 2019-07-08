0d9c0a5a8b7c1dd7624e1c394f81a95bbf0357ed
Mohammad Moien <mnmoienian@gmail.com>
2019-07-08 11:57:03 +0430

    add state for Options component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: ["thing one", "thing two", "thing four"]
+    };
+  }
   render() {
     const title = "Indecision";
     const subtitle = "Put your life in the hands of a computer";
-    const options = ["thing one", "thing two", "thing four"];
 
     return (
       <div>
         <Header title={title} subtitle={subtitle} />
-        <Action />
-        <Options options={options} />
+        <Action hasOptions={this.state.options.length > 0} />
+        <Options options={this.state.options} />
         <AddOption />
       </div>
     );
@@ -33,7 +38,9 @@ class Action extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What should I do?</button>
+        <button onClick={this.handlePick} disabled={!this.props.hasOptions}>
+          What should I do?
+        </button>
       </div>
     );
   }
