b8bb0b86a43897a2996e2f3aa525f6da1bf39acf
Mohammad Moien <mnmoienian@gmail.com>
2019-07-09 10:10:30 +0430

    add default props for page title

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -58,13 +58,12 @@ var IndecisionApp = function (_React$Component) {
   }, {
     key: "render",
     value: function render() {
-      var title = "Indecision";
       var subtitle = "Put your life in the hands of a computer";
 
       return React.createElement(
         "div",
         null,
-        React.createElement(Header, { title: title, subtitle: subtitle }),
+        React.createElement(Header, { subtitle: subtitle }),
         React.createElement(Action, {
           hasOptions: this.state.options.length > 0,
           handlePick: this.handlePick
@@ -96,6 +95,10 @@ var Header = function Header(props) {
       props.subtitle
     )
   );
+};
+
+Header.defaultProps = {
+  title: "Indecision"
 };
 
 var Action = function Action(props) {
--- src/app.js
+++ src/app.js
@@ -36,12 +36,11 @@ class IndecisionApp extends React.Component {
     });
   }
   render() {
-    const title = "Indecision";
     const subtitle = "Put your life in the hands of a computer";
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -63,6 +62,10 @@ const Header = props => {
       <h2>{props.subtitle}</h2>
     </div>
   );
+};
+
+Header.defaultProps = {
+  title: "Indecision"
 };
 
 const Action = props => {
