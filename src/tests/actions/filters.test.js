2958ee9f228403709ef03ae01ca63c181183f658
Mohammad Moien <mnmoienian@gmail.com>
2019-07-25 11:07:21 +0430

    add test for sortByDate action

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,10 @@
 import moment from "moment";
-import { setStartDate, setEndDate, setTextFilter } from "../../actions/filters";
+import {
+  setStartDate,
+  setEndDate,
+  setTextFilter,
+  sortByDate
+} from "../../actions/filters";
 
 test("should generate set start date action object", () => {
   const action = setStartDate(moment(0));
@@ -31,4 +36,8 @@ test("should generate set text filter object with default", () => {
     type: "SET_TEXT_FILTER",
     text: ""
   });
+});
+
+test("should generate action object for sort by date", () => {
+  expect(sortByDate()).toEqual({ type: "SORT_BY_DATE" });
 });
