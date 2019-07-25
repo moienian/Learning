12f75d24ec4d15c11498d4834b2b823ed3a41b85
Mohammad Moien <mnmoienian@gmail.com>
2019-07-25 11:09:57 +0430

    add test for sortByAmount action

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -3,7 +3,8 @@ import {
   setStartDate,
   setEndDate,
   setTextFilter,
-  sortByDate
+  sortByDate,
+  sortByAmount
 } from "../../actions/filters";
 
 test("should generate set start date action object", () => {
@@ -40,4 +41,8 @@ test("should generate set text filter object with default", () => {
 
 test("should generate action object for sort by date", () => {
   expect(sortByDate()).toEqual({ type: "SORT_BY_DATE" });
+});
+
+test("should generate action object for sort by amount", () => {
+  expect(sortByAmount()).toEqual({ type: "SORT_BY_AMOUNT" });
 });
