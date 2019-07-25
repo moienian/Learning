631eeac8171e9a777320aee1412bc6019b599c54
Mohammad Moien <mnmoienian@gmail.com>
2019-07-25 11:03:55 +0430

    add test for setTextFilter action

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,5 @@
 import moment from "moment";
-import { setStartDate, setEndDate } from "../../actions/filters";
+import { setStartDate, setEndDate, setTextFilter } from "../../actions/filters";
 
 test("should generate set start date action object", () => {
   const action = setStartDate(moment(0));
@@ -14,5 +14,21 @@ test("should generate set end date action object", () => {
   expect(action).toEqual({
     type: "SET_END_DATE",
     endDate: moment(0)
+  });
+});
+
+test("should generate set text filter object with text value", () => {
+  const action = setTextFilter("Rent");
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: "Rent"
+  });
+});
+
+test("should generate set text filter object with default", () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
   });
 });
