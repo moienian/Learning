9826a368617dd4f4003ebc4e752dad0f587dc7c7
Mohammad Moien <mnmoienian@gmail.com>
2019-07-25 10:48:41 +0430

    add test for addExpense action

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -18,3 +18,34 @@ test("should setup editExpense action object", () => {
     }
   });
 });
+
+test("should setup addExpense action object", () => {
+  const expenseData = {
+    description: "Rent",
+    amount: 109500,
+    createdAt: 1000,
+    note: "This was last month rent"
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: "ADD_EXPENSE",
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+test("should setup addExpense action object with default value", () => {
+  const action = addExpense();
+  expect(action).toEqual({
+    type: "ADD_EXPENSE",
+    expense: {
+      id: expect.any(String),
+      description: "",
+      note: "",
+      amount: 0,
+      createdAt: 0
+    }
+  });
+});
