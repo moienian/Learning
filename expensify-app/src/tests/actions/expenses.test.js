c1e71512aa058febeed6e048cf1c582beff28218
Mohammad Moien <mnmoienian@gmail.com>
2019-08-11 17:33:23 +0430

    add startEditExpense for handle edit to firebase

--- expensify-app/src/tests/actions/expenses.test.js
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -4,6 +4,7 @@ import {
   startAddExpense,
   addExpense,
   editExpense,
+  startEditExpense,
   removeExpense,
   startRemoveExpense,
   setExpenses,
@@ -61,6 +62,27 @@ test("should setup editExpense action object", () => {
       note: "new note value"
     }
   });
+});
+
+test("should edit expense from firebase", done => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 21045 };
+  store
+    .dispatch(startEditExpense(id, updates))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "EDIT_EXPENSE",
+        id,
+        updates
+      });
+      return database.ref(`expense/${id}`).once("value");
+    })
+    .then(snapshot => {
+      expect(snapshot.val().amount).toBe(updates.amount);
+      done();
+    });
 });
 
 test("should setup addExpense action object", () => {
