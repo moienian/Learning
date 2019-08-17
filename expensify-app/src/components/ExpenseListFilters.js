dce3865826e35fa8f0177c24c29c3bb2957367fd
Mohammad Moien <mnmoienian@gmail.com>
2019-08-17 17:41:15 +0430

    add style for inputs

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -40,15 +40,17 @@ export class ExpenseListFilters extends React.Component {
       <div className="content-container">
         <div className="input-group">
           <div className="input-group__item">
-            {" "}
             <input
               type="text"
+              className="text-input"
+              placeholder="Search Expenses"
               value={this.props.filters.text}
               onChange={this.onTextChange}
             />
           </div>
           <div className="input-group__item">
             <select
+              className="select"
               value={this.props.filters.sortBy}
               onChange={this.onSortChange}
             >
