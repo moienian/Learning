957d0fa8e23454434b324f66b57172a713512d5e
Mohammad Moien <mnmoienian@gmail.com>
2019-09-04 16:00:55 +0430

    config webpack

---
+++ redux-counter/src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+
+const root = document.getElementById("root");
+const el = React.createElement("p", null, "test");
+ReactDOM.render(el, root);
