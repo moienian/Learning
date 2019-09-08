8fdfc55158a0cb76276fb8431e824c7a4c7e2015
Mohammad Moien <mnmoienian@gmail.com>
2019-09-08 11:19:48 +0430

    add class properties plugin for babel

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,21 +1,15 @@
 import React from "react";
 
 class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.incrementIfOdd = this.incrementIfOdd.bind(this);
-    this.incrementAsync = this.incrementAsync.bind(this);
-  }
-
-  incrementIfOdd() {
+  incrementIfOdd = () => {
     if (this.props.value % 2 !== 0) {
       this.props.onIncrement();
     }
-  }
+  };
 
-  incrementAsync() {
+  incrementAsync = () => {
     setTimeout(this.props.onIncrement, 1000);
-  }
+  };
 
   render() {
     const { value, onIncrement, onDecrement } = this.props;
