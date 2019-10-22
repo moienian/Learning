f5eebf12ddb80814adcc04cfe765d1c8f8f5d7ca
Mohammad Moien <mnmoienian@gmail.com>
2019-10-22 09:28:59 +0330

    Init react-component

---
+++ react-components/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public", "dist"),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    historyApiFallback: true,
+    open: true,
+    watchContentBase: true
+  }
+};
